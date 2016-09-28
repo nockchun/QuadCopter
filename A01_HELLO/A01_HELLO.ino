540d938fdea58dc33f547f1b4956918ed2e508de
albertx <albertx@realstudy.net>
2016-09-29 04:12:20 +0900

    hello world create

---
+++ A01_HELLO/A01_HELLO.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void setup() {
+	Serial.begin(19200);	// init serial communication (data rate : 19200)
+}
+
+void loop() {
+	Serial.println("Hello World.");
+	Serial.println(Serial.available());
+	Serial.println(Serial);
+
+	Serial.print(78, BIN);
+	Serial.println(78, OCT);
+	Serial.println(78, DEC);
+	Serial.println(78, HEX);
+
+	Serial.println(4.14, 0);
+	Serial.println(4.14, 1);
+
+	Serial.println("");
+
+	delay(5000);
+}
